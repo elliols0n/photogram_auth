b6ea7cb5fe0f52907e138d3041574a769e0af295
elli olson <eolson10@chicagobooth.edu>
2017-08-14 02:20:23 +0000

    first push working off of recording

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index, :show]
+
+  def index
+    @users = User.all
+
+    render("users/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render("users/show.html.erb")
+  end
+
+end
